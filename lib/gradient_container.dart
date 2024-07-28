3807a9312bbda1cb5cdb4c8333b60f458af34947
Akila Mendis <godomainz@gmail.com>
2024-07-28 11:50:31 +0530

    46. Adding Buttons & Using Functions As Values

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 var startAlignment = Alignment.topLeft;
@@ -6,6 +8,10 @@ var endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   final List<Color> colors;
   const GradientContainer({super.key, required this.colors});
+
+  void rollDice() {
+    log("Button pressed");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +24,14 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          "assets/images/dice-2.png",
-          width: 200,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/dice-2.png",
+              width: 200,
+            ),
+            TextButton(onPressed: rollDice, child: const Text("Roll Dice")),
+          ],
         ),
       ),
     );
